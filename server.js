d1add19034d5be6c791d1e6608c42252d7a0ad28
Martinprosecky <16.martin.prosecky.16@gmail.com>
2025-01-02 19:07:01 +0100

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const mongoose = require("mongoose");
 const app = express();
 const port = 3000;
 
+app.get("/", (req, res) => {
+    res.send("Server běží správně!"); // Odpověď, kterou uvidíte na adrese /
+});
+
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
